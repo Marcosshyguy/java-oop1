7565a7d6f4089fd7a4372120b97b605e1c4d2a1f
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-10 17:49:15 +0100

    Add:user choices

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Bank {
     public static void main(String[] args) {
+        boolean flag = false;
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Add your name: ");
         String newCostumer = scan.nextLine();
@@ -11,11 +13,36 @@ public class Bank {
         Conto newBankAccount = new Conto(newCostumer);
         System.out.println(newBankAccount.toString());
 
-        /*newBankAccount.storeMoney(40.50);
-        newBankAccount.storeMoney(30);
-        newBankAccount.getMoney(100);*/
-        System.out.println(newBankAccount.getBankBalance());
 
+        while (!flag) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Make a choice");
+            System.out.println("Select 1 if you want to deposit money");
+            System.out.println("Select 2 if you want to take money");
+            System.out.println("Select 3 to exit");
+            int choice = input.nextInt();
+
+            if (choice == 1){
+                System.out.println("How much money do you want to deposit?");
+                double moneyDeposited = input.nextDouble();
+                newBankAccount.storeMoney(moneyDeposited);
+                System.out.println("Now you have: " + newBankAccount.getBankBalance()+ "$");
+            } else if (choice == 2) {
+                System.out.println("How much money do you want to take?");
+                double moneyTook = input.nextDouble();
+                newBankAccount.getMoney(moneyTook);
+                System.out.println("Now you have: " + newBankAccount.getBankBalance()+ "$");
+            }else if(choice == 3){
+                flag = true;
+                System.out.println("Bye bye!");
+            }else {
+                System.out.println("Wrong choice");
+            }
+        }
 
     }
 }
+
+
+
